389c78ee4ae1390343ab87ad6c26eea81f86136b
Tiểu Bạch Long <41384348+tieubachlongblog@users.noreply.github.com>
2021-10-22 11:26:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,9 @@
 const nameGirl = 'Em';
-const giftUrl = 'http://nodemy.vn';
+const giftUrl = '';
 const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const titleCard = 'Tặng Em :>';
+const contentCard = 'Chúc Em 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Em trong hôm nay và cả những ngày sau Wish you all the best <3';
 
-// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
-// Cần hỗ trợ hãy liên hệ: 
-// Mr-Nam http://facebook.com/nam.nodemy
-// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
 const giftImage = 'hot-girl.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
