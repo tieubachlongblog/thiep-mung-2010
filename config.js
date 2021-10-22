b626ca34bf3c4bb5bd88f29cca3db16362a87b0c
Tiểu Bạch Long <41384348+tieubachlongblog@users.noreply.github.com>
2021-10-22 11:27:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Em';
 const giftUrl = '';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng Em :>';
-const contentCard = 'Chúc Em 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Em trong hôm nay và cả những ngày sau Wish you all the best <3';
+const contentCard = 'Chúc Em 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Em trong hôm nay và cả những ngày sau. Wish you all the best <3';
 
 const giftImage = 'hot-girl.png';
 const base64 = '';
